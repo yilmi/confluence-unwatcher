b6313aff2bba5c59927729937e9624e759918b08
Pedro Laguna <plaguna@vmware.com>
2019-10-14 11:10:54 +0100

    Improved JavaScript formatting
    I made the js formatting consistent across files to improve readability.

--- content.js
+++ content.js
@@ -1,24 +1,24 @@
 // Function disabling notify watchers in the DOM
-function unWatch(){
+function unWatch() {
     document.getElementById("notifyWatchers").checked = false;
     console.log("The 'Notify watchers' checkbox was set to false on this page");
 }
 
 // If we have a notifyWatchers element, let's disable it
-if(document.getElementById("notifyWatchers")) {
-    chrome.runtime.sendMessage({status: "check"}, function(response) {
-        if(response.status == true){
+if (document.getElementById("notifyWatchers")) {
+    chrome.runtime.sendMessage({ status: "check" }, function (response) {
+        if (response.status == true) {
             unWatch();
         }
     });
 }
 // Otherwise, we setup a listener, when a display page switches to edit mode, we'll get an event
-document.getElementById("content").addEventListener("DOMSubtreeModified", function(event){
-    if(event.target.id != 'notifyWatchers' ) {
+document.getElementById("content").addEventListener("DOMSubtreeModified", function (event) {
+    if (event.target.id != 'notifyWatchers') {
         return false;
     };
-    chrome.runtime.sendMessage({status: "check"}, function(response) {
-        if(response.status == true){
+    chrome.runtime.sendMessage({ status: "check" }, function (response) {
+        if (response.status == true) {
             unWatch();
         }
     });
